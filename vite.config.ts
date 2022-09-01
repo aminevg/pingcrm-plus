d1116976a3c648ef8359b80801a1d94e121f2669
Amine Ilidrissi <38422328+aminevg@users.noreply.github.com>
2022-09-01 17:58:12 +0900

    Remove unneeded import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
 
 export default defineConfig({
     plugins: [
