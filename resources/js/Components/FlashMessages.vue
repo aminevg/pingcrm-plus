4ef4e6b9cb43bddce014f0b85d30afc786ea6d87
Amine Ilidrissi <38422328+aminevg@users.noreply.github.com>
2022-09-07 23:40:38 +0900

    Restyle flash messages

--- resources/js/Components/FlashMessages.vue
+++ resources/js/Components/FlashMessages.vue
@@ -15,76 +15,94 @@ const errorsLength = computed(() => (errors ? Object.keys(errors).length : 0));
 </script>
 
 <template>
-    <div>
+    <div class="mb-4 flex flex-col items-center gap-y-4">
         <div
             v-if="flash?.success && show"
-            class="flex items-center justify-between mb-8 max-w-3xl bg-green-500 rounded"
+            class="alert alert-success flex-row shadow-lg md:max-w-xl md:w-1/2"
         >
-            <div class="flex items-center">
+            <div>
                 <svg
-                    class="flex-shrink-0 ml-4 mr-2 w-4 h-4 fill-white"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                >
-                    <polygon points="0 11 2 9 7 14 18 3 20 5 7 18" />
-                </svg>
-                <div class="py-4 text-white text-sm font-medium">
-                    {{ flash.success }}
-                </div>
-            </div>
-            <button type="button" class="group mr-2 p-2" @click="show = false">
-                <svg
-                    class="block w-2 h-2 fill-green-800 group-hover:fill-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="235.908"
-                    height="235.908"
-                    viewBox="278.046 126.846 235.908 235.908"
+                    class="stroke-current flex-shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
                 >
                     <path
-                        d="M506.784 134.017c-9.56-9.56-25.06-9.56-34.62 0L396 210.18l-76.164-76.164c-9.56-9.56-25.06-9.56-34.62 0-9.56 9.56-9.56 25.06 0 34.62L361.38 244.8l-76.164 76.165c-9.56 9.56-9.56 25.06 0 34.62 9.56 9.56 25.06 9.56 34.62 0L396 279.42l76.164 76.165c9.56 9.56 25.06 9.56 34.62 0 9.56-9.56 9.56-25.06 0-34.62L430.62 244.8l76.164-76.163c9.56-9.56 9.56-25.06 0-34.62z"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                </svg>
+                <span>
+                    {{ flash.success }}
+                </span>
+            </div>
+            <button
+                type="button"
+                class="btn btn-ghost btn-xs !mt-0"
+                @click="show = false"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
                     />
                 </svg>
             </button>
         </div>
         <div
             v-if="(flash?.error || errorsLength > 0) && show"
-            class="flex items-center justify-between mb-8 max-w-3xl bg-red-500 rounded"
+            class="alert alert-error flex-row shadow-lg md:max-w-xl md:w-1/2"
         >
-            <div class="flex items-center">
+            <div>
                 <svg
-                    class="flex-shrink-0 ml-4 mr-2 w-4 h-4 fill-white"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
+                    class="stroke-current flex-shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
                 >
                     <path
-                        d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm1.41-1.41A8 8 0 1 0 15.66 4.34 8 8 0 0 0 4.34 15.66zm9.9-8.49L11.41 10l2.83 2.83-1.41 1.41L10 11.41l-2.83 2.83-1.41-1.41L8.59 10 5.76 7.17l1.41-1.41L10 8.59l2.83-2.83 1.41 1.41z"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
                     />
                 </svg>
-                <div
-                    v-if="flash?.error"
-                    class="py-4 text-white text-sm font-medium"
-                >
+                <span v-if="flash?.error">
                     {{ flash.error }}
-                </div>
-                <div v-else class="py-4 text-white text-sm font-medium">
-                    <span v-if="errorsLength === 1"
-                        >There is one form error.</span
-                    >
-                    <span v-else
-                        >There are {{ errorsLength }} form errors.</span
-                    >
-                </div>
+                </span>
+                <span v-else>
+                    There
+                    {{ errorsLength === 1 ? "is one" : `are ${errorsLength}` }}
+                    form errors.
+                </span>
             </div>
-            <button type="button" class="group mr-2 p-2" @click="show = false">
+            <button
+                type="button"
+                class="btn btn-ghost btn-xs !mt-0"
+                @click="show = false"
+            >
                 <svg
-                    class="block w-2 h-2 fill-red-800 group-hover:fill-white"
                     xmlns="http://www.w3.org/2000/svg"
-                    width="235.908"
-                    height="235.908"
-                    viewBox="278.046 126.846 235.908 235.908"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
                 >
                     <path
-                        d="M506.784 134.017c-9.56-9.56-25.06-9.56-34.62 0L396 210.18l-76.164-76.164c-9.56-9.56-25.06-9.56-34.62 0-9.56 9.56-9.56 25.06 0 34.62L361.38 244.8l-76.164 76.165c-9.56 9.56-9.56 25.06 0 34.62 9.56 9.56 25.06 9.56 34.62 0L396 279.42l76.164 76.165c9.56 9.56 25.06 9.56 34.62 0 9.56-9.56 9.56-25.06 0-34.62L430.62 244.8l76.164-76.163c9.56-9.56 9.56-25.06 0-34.62z"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
                     />
                 </svg>
             </button>
